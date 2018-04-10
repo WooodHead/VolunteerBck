61d3078d79613508175b0c1907d00cc610e8ce23
ropaillet <ropaillet@gmail.com>
2018-04-10 18:06:14 -0300

    Schema Updates

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,8 +1,7 @@
 import { makeExecutableSchema } from 'graphql-tools';
-import { MongoClient, ObjectId } from 'mongodb';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import { Resolvers } from './resolvers';
 
+//Type Definitions
 const typeDefs = [`
   type User {
     _id: String
@@ -68,123 +67,7 @@ const typeDefs = [`
   }
 `];
 
-const resolvers = {
-  Query: {
-    currentUser: (root, args, { user }) => {
-      return user;
-    },
-    ngosByName: async (root, { name }, { mongo }) => {
-      const Ngos = mongo.collection('ngos');
-      const ngosByName = await Ngos.findOne({ name });
-      console.log(ngosByName);
-      return ngosByName;
-    }
-  },
-  Mutation: {
-    signup: async (root, { email, password }, { mongo }) => {
-
-      const Users = mongo.collection('users');
-      const existingUser = await Users.findOne({ email });
-
-      if (existingUser) {
-        throw new Error('Email already used');
-      }
-
-      const hash = await bcrypt.hash(password, 10);
-
-      await Users.insert({
-        email,
-        password: hash,
-      });
-
-      const user = await Users.findOne({ email });
-      user.jwt = jwt.sign({ _id: user._id }, process.env.JWT_SECRET );
-      console.log('User created');
-      return user;
-    },
-
-    createNGO: async (root, { name, mission }, { mongo }) => {
-      const Ngos = mongo.collection('ngos');
-      await Ngos.insert({
-        name,
-        mission,
-      });
-      const ngo = await Ngos.findOne({ name });
-      console.log(name + ' NGO Added');
-      return ngo;
-    },
-
-    login: async (root, { email, password }, { mongo }) => {
-      const Users = mongo.collection('users');
-
-      const user = await Users.findOne({ email });
-      if (!user) {
-        throw new Error ('Email does not exist, register first');
-      }
-      const validPassword = await bcrypt.compare(password, user.password);
-
-      if (!validPassword) {
-        throw new Error ('Password is incorrect');
-      }
-      user.jwt = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-      console.log('User ' + email + ' Logged In');
-      return user;
-    },
-  }
-};
-
-const getUser = async (authorization, mongo) => {
-  const bearerLength = "Bearer ".length;
-
-  if (authorization && authorization.length > bearerLength) {
-    const token = authorization.slice(bearerLength);
-
-    const { ok, result } = await new Promise(resolve =>
-  		jwt.verify(token, process.env.JWT_SECRET , (err, result ) => {
-      	if (err){
-          console.log(err);
-          resolve({
-            ok: false,
-            result: err,
-          })
-        }	else {
-          resolve({
-            ok: true,
-            result,
-          })
-        }
-    	})
-    );
-    if (ok) {
-      const user = await mongo.collection('users').findOne({ _id: Object(result._id)});
-      return user;
-    }	else {
-      return null;
-    }
-  }
-  return null;
-};
-
-
-let mongo;
-let client;
-
-export async function context(headers) {
-  if(!mongo) {
-    client = await MongoClient.connect(process.env.MONGO_URI);
-  	mongo = client.db('volunteer');
-  }
-
-  const user = await getUser(headers['authorization'], mongo );
-  return {
-    headers,
-    mongo,
-    user,
-  };
-};
-
-export const schema = makeExecutableSchema({
+export const executableSchema = makeExecutableSchema({
   typeDefs,
-  resolvers,
-  context
+  resolvers: Resolvers,
 })
--- server/src/server.js
+++ server/src/server.js
@@ -2,57 +2,45 @@ require('dotenv').config()
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import jwt from 'express-jwt';
+import { MongoClient, ObjectId } from 'mongodb';
 import * as bodyParser from 'body-parser-graphql'
-import * as Schema from './schema';
+import { executableSchema } from './schema';
 
 const PORT = 3000;
 const server = express();
-
-const schemaFunction =
-  Schema.schemaFunction ||
-  function() {
-    return Schema.schema;
-  };
-
-let schema;
-
-const rootFunction =
-  Schema.rootFunction ||
-  function() {
-    return schema.rootValue;
-  };
-
-const contextFunction =
-  Schema.context ||
-  function(headers) {
-    return Object.assign(
-      {
-        headers: headers,
-      },
-    );
-  };
 
 server.use('/graphql', jwt({
   secret: process.env.JWT_SECRET,
   credentialsRequired: false,
 }), bodyParser.graphql(), graphqlExpress(async req => {
-  if(req.user) {
-    console.log(req.user)
-  }
-  if (!schema) {
-    schema = schemaFunction(process.env)
-  }
-  const context = await contextFunction(req.headers, process.env);
-  const rootValue = await rootFunction(req.headers, process.env);
 
+  //Connect to mongo db database
+  let mongo;
+  if(!mongo) {
+    const client = await MongoClient.connect(process.env.MONGO_URI);
+    mongo = client.db('volunteer');
+  }
+
+  const Users = mongo.collection('users');
+  
+  //Log req.user for testing purposes
+  if (req.user) {
+    console.log(req.user);
+  }
+
+  //Return schema and context with DB connection and user if exists.
   return {
-    schema: await schema,
-    rootValue,
-    context,
-    tracing: true,
+    schema: executableSchema,
+    context: {
+      mongo: mongo,
+      user: req.user ?
+        Users.findOne({ where: { id: req.user.id }}) :
+        Promise.resolve(null),
+    },
   };
 }));
 
+//GraphiQL End point for testing (no token);
 server.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
   query: ``,
@@ -60,8 +48,5 @@ server.use('/graphiql', graphiqlExpress({
 
 server.listen(PORT, () => {
   console.log(`Volunteer GraphQL Server is running on http://localhost:${PORT}/graphql`);
-  console.log(`Volunteer View GraphiQL at http://localhost:${PORT}/graphiql`);
+  console.log(`Volunteer GraphiQL Interface running at http://localhost:${PORT}/graphiql`);
 });
-
-
-//
