3c62c655be882b1c794b4462374b01e99e9781ff
ropaillet <ropaillet@gmail.com>
2018-04-12 13:31:10 -0300

    Helpers Modifications
    a

--- server/src/helpers.js
+++ server/src/helpers.js
@@ -1,16 +1,16 @@
 export function validateEmail (email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
 
 export function validatePassword (password) {
   //At least 1 letter
-  var letter = /^(?=.*?[a-z])/;
+  const letter = /^(?=.*?[a-z])/;
   //At least 1 special char
-  var specialChar = /^(?=.*[!@#$%^&*])/;
+  const specialChar = /^(?=.*[!@#$%^&*])/;
   //At Least 8 CHaracters
-  var fiveCharacters = /^.{5,}$/;
+  const fiveCharacters = /^.{5,}$/;
 
   //Validation
   if (!letter.test(String(password)) || !fiveCharacters.test(String(password))) {
