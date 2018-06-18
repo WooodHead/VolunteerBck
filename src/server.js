8eaf18891689a850c8cfeb319beba35f6bdd4e2c
ropaillet <ropaillet@gmail.com>
2018-06-18 16:41:35 -0300

    Push to Heroku

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 import express from 'express';
 // import { ApolloEngine } from 'apollo-engine'
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { graphqlExpress } from 'apollo-server-express';
 import { MongoClient, ObjectId } from 'mongodb';
 import { apolloUploadExpress } from 'apollo-upload-server'
 import jwt from 'express-jwt';
@@ -34,7 +34,7 @@ getMongo();
 
 // Initialize the server
 server.use(
-  '/graphql', apolloUploadExpress(),cors(), jwt({
+  '/', apolloUploadExpress(),cors(), jwt({
   secret: process.env.JWT_SECRET,
   credentialsRequired: false,
 }), bodyParser.graphql(), graphqlExpress(async req => {
@@ -49,13 +49,12 @@ server.use(
   };
 }));
 
-//GraphiQL End point for testing (no token);
-server.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-  query: ``,
-}));
+// //GraphiQL End point for testing (no token);
+// server.use('/graphiql', graphiqlExpress({
+//   endpointURL: '/graphql',
+//   query: ``,
+// }));
 
 server.listen(PORT, () => {
   console.log(`Volunteer GraphQL Server is running on http://localhost:${PORT}/graphql`);
-  console.log(`Volunteer GraphiQL Interface running at http://localhost:${PORT}/graphiql`);
 });
